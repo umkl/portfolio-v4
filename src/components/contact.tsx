f7c4de2c594179b70ed6e80fd6122a9b324d4c19
Michael Ungar <michael.ungar03@gmail.com>
2023-11-21 14:17:57 +0100

    contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const ContactSection = () => {
 	return (
-		<div className="w-full h-screen bg-red-200 flex snap-start relative">
-			<div className="w-[800px] h-full art-bg flex justify-center items-center">
+		<div className="w-full min-h-screen flex sm:flex-row h-fit flex-col snap-start relative art-bg">
+			<div className="sm:w-[600px] w-full  h-[1000px] shrink-0 flex justify-center items-center">
 				<div className="w-full h-fit mx-14">
 					<h1 className="text-2xl font-druk text-[#D9C2A7] inset-8">
 						contact me
@@ -16,13 +16,13 @@ const ContactSection = () => {
 						<input
 							type="text"
 							placeholder="Deine Email"
-							className=" bg-[#4a4a4a44] backdrop-blur-sm focus:ring-[#d9c2a7c3]  focus:outline-none ring-2 ring-[#4a4a4aad] focus:ring-4 focus:border-teal focus:border-red-500 outline-teal-200 caret-[#d9c2a7c3]  rounded-md w-full mt-2 p-4 font-mono "
+							className=" bg-[#4a4a4a44] backdrop-blur-sm focus:ring-[#d9c2a7c3]  focus:outline-none ring-2 ring-[#4a4a4aad] focus:ring-4 focus:border-teal  outline-teal-200 caret-[#d9c2a7c3]  rounded-md w-full mt-2 p-4 font-mono "
 						/>
 						<textarea
 							placeholder="Verfasse hier eine nette Nachricht an mich :)"
 							rows={4}
 							cols={50}
-							className=" bg-[#4a4a4a44] backdrop-blur-sm focus:ring-[#d9c2a7c3]  focus:outline-none ring-2 ring-[#4a4a4aad] focus:ring-4 focus:border-teal focus:border-red-500 outline-teal-200 caret-[#d9c2a7c3] h-[200px] rounded-md w-full mt-4 p-4 font-mono "
+							className=" bg-[#4a4a4a44] backdrop-blur-sm focus:ring-[#d9c2a7c3]  focus:outline-none ring-2 ring-[#4a4a4aad] focus:ring-4 focus:border-teal  outline-teal-200 caret-[#d9c2a7c3] h-[200px] rounded-md w-full mt-4 p-4 font-mono "
 						/>
 						<div className="w-full flex">
 							<input
@@ -33,7 +33,7 @@ const ContactSection = () => {
 						</div>
 					</form>
 				</div>
-				<div className="absolute translate-x-2 bottom-[50px]  h-fit flex ">
+				<div className=" absolute translate-x-2 bottom-[50px] hidden  h-fit sm:flex ">
 					<Image
 						src="/logo-skeleton.svg"
 						alt="Vercel Logo"
@@ -49,10 +49,26 @@ const ContactSection = () => {
 					</div>
 				</div>
 			</div>
-			<div className="w-full h-full contact-image relative">
-				<h2 className="absolute bottom-10 right-10 font-mono italic text-gray-400">
+
+			<div className="sm:flex-1  contact-image shrink-0 h-[1000px] relative">
+				<h2 className="absolute bottom-10 right-10 font-mono italic text-gray-400 ">
 					my first coding setup, 2018
 				</h2>
+			</div>
+			<div className=" relative sm:hidden  shrink-0  h-fit my-16 flex  justify-center">
+				<Image
+					src="/logo-skeleton.svg"
+					alt="Vercel Logo"
+					className=""
+					objectFit=""
+					height={60}
+					width={60}
+					priority
+				/>
+				<div className="h-full w-full relative flex flex-col justify-center ml-4 ">
+					<h2 className="font-mono">made with &lt;3</h2>
+					<h2 className="font-mono">by ungarmichael</h2>
+				</div>
 			</div>
 		</div>
 	);
