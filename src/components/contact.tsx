b9932c2d2274c7486f9c9bbf325cba80b0b9a7fd
um <michael.ungar03@gmail.com>
2023-12-07 17:55:08 +0100

    input fields

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -24,31 +24,43 @@ const ContactSection = () => {
 				</h2>
 				<div className="h-[3em]"></div>
 				<form>
-					<input
-						type="text"
-						id="name"
-						placeholder="Your Name"
-						className="w-full border-t-0 focus:border-none border-white border-b-2 text-2xl font-clash bg-transparent font-bold  my-2 pb-2 placeholder:text-white selection:border-none focus:outline-none "
-					/>
-					<label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 2xl:text-body-2 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75">
-						YOUR NAME
-					</label>
-					<input
-						type="text"
-						value="Your Name"
-						placeholder="Your Name"
-						className="w-full border-none border-b-2 text-2xl font-clash bg-transparent font-bold border-white"
-					/>
 					<div className="relative z-0">
 						<input
 							type="text"
 							id="fsd"
-							className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer font-clash"
+							className="block py-2.5 px-0 w-full text-[1.2em] text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white   focus:outline-none focus:ring-0 focus:border-white peer font-clash"
 							placeholder=" "
 						/>
 						<label
 							htmlFor="fsd"
-							className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto font-clash"
+							className="absolute text-[1.2em] text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto font-clash"
+						>
+							Floating standard
+						</label>
+					</div>
+					<div className="relative z-0">
+						<input
+							type="text"
+							id="fsd"
+							className="block py-2.5 px-0 w-full text-[1.2em] text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white   focus:outline-none focus:ring-0 focus:border-white peer font-clash"
+							placeholder=" "
+						/>
+						<label
+							htmlFor="fsd"
+							className="absolute text-[1.2em] text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto font-clash"
+						>
+							Floating standard
+						</label>
+					</div>
+					<div className="relative z-0">
+						<textarea
+							id="fsd"
+							className="block py-2.5 px-0 w-full text-[1.2em] text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white   focus:outline-none focus:ring-0 focus:border-white peer font-clash"
+							placeholder=" "
+						/>
+						<label
+							htmlFor="fsd"
+							className="absolute text-[1.2em] text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto font-clash"
 						>
 							Floating standard
 						</label>
