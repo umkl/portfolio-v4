0f6797a340d9b360b6ea22019c3b8f2831b58821
Michael <michael.ungar03@gmail.com>
2024-02-14 21:42:43 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,41 @@ import ExperienceSection from "@um-p4/components/experienceSection";
 import ProjectsSection from "@um-p4/components/projectsSection";
 
 import { NavContext } from "../navcontext";
+import { Metadata } from "next";
+import Head from "next/head";
 
 export default function Home() {
 	return (
-		<Curve>
-			<Landing />
-		</Curve>
+		<>
+			<Head>
+				<title>Ungar Michael - Freelance App Developer & UI/UX Designer</title>
+				<meta
+					name="description"
+					content="Landing Page to present conducted projects in App Development, UI/UX Design and Website Coding."
+					key="desc"
+				/>
+				<meta
+					property="og:title"
+					content="Ungar Michael - Freelance App Developer & UI/UX Designer"
+				/>
+				<meta
+					property="og:description"
+					content="Landing Page to present conducted projects in App Development, UI/UX Design and Website Coding."
+				/>
+				<meta property="og:image" content="/banner.png" />
+			</Head>
+			<Curve>
+				<Landing />
+			</Curve>
+		</>
 	);
 }
+
+export const metadata: Metadata = {
+	title: {
+		template: "%s | Acme Dashboard",
+		default: "Acme Dashboard",
+	},
+	description: "The official Next.js Learn Dashboard built with App Router.",
+	metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
